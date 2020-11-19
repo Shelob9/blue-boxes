c76439e906c2d3750ffd4dc70d63b3e65d81ce25
Josh Pollock <jpollock412@gmail.com>
2020-11-19 18:30:18 -0500

    hi roy commit

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,12 @@ function MyApp({ Component, pageProps }) {
   const [user, setUser] = useState()
 
   useEffect(() => {
+
     userbase.init({ appId: process.env.NEXT_PUBLIC_USERBASE_APP_ID })
+      .then((session) => {
+        console.log(session.user);
+      });
+ 
   }, [])
 
   return (
