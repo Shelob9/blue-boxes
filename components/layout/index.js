d0fa5c95df5b56efe843087acac41f623cd22419
Josh Pollock <jpollock412@gmail.com>
2020-11-19 19:28:37 -0500

    add footer

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,10 +1,14 @@
 import Nav from '../nav'
 function Layout({ user, setUser, children }) {
-  console.log(user);
   return (
     <div className="container mx-auto">
       <Nav user={user} setUser={setUser} />
       {children}
+      <footer className={'mt-8'}>
+        <p >
+          Something <a href="https://joshpress.net">Josh</a> might be making. Got the idea from <a href="https://twitter.com/visakanv/status/1329093575649370113">this Vias tweet.</a>
+        </p>
+      </footer>
     </div>
   )
 }
