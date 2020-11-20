3834e7b74cd28dba4580fc620cf1a4ef5d530fdb
Josh Pollock <jpollock412@gmail.com>
2020-11-20 10:39:09 -0500

    more work with userbase

--- pages/graphs.tsx
+++ pages/graphs.tsx
@@ -3,57 +3,97 @@ import userbase,{ UserProfile } from "userbase-js";
 import Graph, { IGraph, makeGraph } from "../components/Graph";
 import { useRouter } from 'next/router'
 
-export async function getServerSideProps() {
-    const graph = makeGraph();
+export async function getServerSideProps({query}) {
+    //const graph = makeGraph();
+    const { graphId } = query;
     return {
-        props: { graph },
+        props: { graphId },
     };
 };
 
-const useGraphDb = ({ _graph }) => {
+export interface ISaveGraph {
+    graphId: string;
+    rows: string[]
+}
+
+const mapRows = (saveGraph:ISaveGraph): IGraph => {
+    const { graphId } = saveGraph;
+    let rows = {};
+    saveGraph.rows.forEach(rowId => {
+        rows[rowId] = {
+            rowId,
+            boxes:{}
+        }
+    })
+    return {
+        graphId,
+        rows
+    }
+}
+const useGraphDb = ({ graphId }) => {
     const [isLoaded, setIsLoaded] = useState(false);
-    const [graph, setGraph] = useState<IGraph>(_graph);
+    const [loadingState, setLoadingState] = useState({
+        graph: false,
+        rows: false,
+        boxes: false,
+    })
+    const [graph, setGraph] = useState<IGraph>();
+
+    //Open graph database and load the saved graph
     useEffect(() => {
         async function openDatabase() {
             try {
                 await userbase.openDatabase({
                     databaseName: `graphs`,
                     changeHandler: function (items) {
-                        console.log(items);
+                        let g = items.find(item => item.itemId === graphId);
+                        if (g) {
+                            //@ts-ignore
+                            setGraph(mapRows(g.item));
+                            setLoadingState({...loadingState,graph:true})
+                        }
                     },
                 });
-                setIsLoaded(true);
             
             } catch (e) {
                 console.error(e.message)
             }
         }
-        openDatabase()
+        openDatabase();
     }, []);
     useEffect(() => {
-        if (isLoaded) {
-            userbase.insertItem({
-                databaseName: `graphs`,
-                item: { graphId: graph.graphId },
-                itemId: graph.graphId
-            }).then((r) => {
-                 
-            }).catch((e) => console.log(e));
+        async function openRowsDatabase() {
+            try {
+                await userbase.openDatabase({
+                    databaseName: `graph-rows`,
+                    changeHandler: function (items) {
+                        console.log(items);
+                    },
+                });
+            
+            } catch (e) {
+                console.error(e.message)
+            }
         }
-    }, [isLoaded]);
+
+        if (true === loadingState.graph && false === loadingState.rows) {
+            
+            openRowsDatabase();
+       } 
+    }, [loadingState]);
     return {
         graph
     }
 }
-const Graphs = (props: { graph: IGraph; user?: UserProfile }) => {
-    let { graph } = useGraphDb({ _graph: props.graph });
+const Graphs = (props: { graphId?: string; user?: UserProfile }) => {
+    let { graph } = useGraphDb({ graphId: props.graphId });
     const router = useRouter()
-    
     useEffect(() => {
-        if (graph.graphId) {
+        if (graph && graph.graphId) {
            router.push(`${router.pathname}?graphId=${graph.graphId}`)
         }
-    },[router.pathname]);
+    }, [router.pathname]);
+    return <div>1</div>
     return (
         <div>
             <h1>
